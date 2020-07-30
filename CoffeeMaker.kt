834c02b5e800c65ba67e57debcec5d754f813ad3
watanavex <y_watanabe@yumemi.co.jp>
2020-07-30 16:46:31 +0900

    CoffeeMakerからHeaterを利用するPumpを実装

--- CoffeeMaker.kt
+++ CoffeeMaker.kt
@@ -1,10 +1,13 @@
 
 class CoffeeMaker(
     val header: Heater, 
+    val pump: Pump
     ) {
 
     fun brew() {
         header.on()
+        pump.pump()
+        print("☕️")
         header.off()
     }
 
